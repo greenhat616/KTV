ffd27834f6a07e3c6fe1ca589f1a0c6fb147b632
greenhat616 <i@a632079.me>
2022-06-20 21:44:54 +0800

    chore: ignore workdir

--- src/main/java/me/a632079/ktv/views/OrderSongByNameWindow.java
+++ src/main/java/me/a632079/ktv/views/OrderSongByNameWindow.java
@@ -2,11 +2,15 @@ package me.a632079.ktv.views;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JTextField;
 
 public class OrderSongByNameWindow {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -36,8 +40,22 @@ public class OrderSongByNameWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 483, 614);
+		frame.setTitle("歌名点歌");
+		frame.setBounds(100, 100, 483, 625);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
 
+		JList list = new JList();
+		list.setBounds(10, 10, 449, 507);
+		frame.getContentPane().add(list);
+
+		textField = new JTextField();
+		textField.setBounds(10, 538, 341, 40);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(361, 538, 98, 40);
+		frame.getContentPane().add(btnNewButton);
+	}
 }
